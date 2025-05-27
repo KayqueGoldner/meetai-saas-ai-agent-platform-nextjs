9d00429d882be7f57ba22dd5659ca13d206f07a6
Kayque Goldner <kayquegoldner@gmail.com>
2025-05-27 16:50:32 -0300

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold text-green-500 underline">
+        Hello World
+      </h1>
+      <Button variant="outline">Click me</Button>
+    </div>
+  );
+}
