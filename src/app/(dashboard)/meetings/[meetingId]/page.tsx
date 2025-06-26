66633a62c46f03768b34367494b2c07bfb864a97
Kayque Goldner <kayquegoldner@gmail.com>
2025-06-26 06:32:41 -0300

    meeting details page

--- src/app/(dashboard)/meetings/[meetingId]/page.tsx
+++ src/app/(dashboard)/meetings/[meetingId]/page.tsx
@@ -1,5 +1,47 @@
-const MeetingIdPage = () => {
-  return <div>MeetingIdPage</div>;
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+
+import { auth } from "@/lib/auth";
+import { getQueryClient, trpc } from "@/trpc/server";
+import {
+  MeetingIdView,
+  MeetingIdViewError,
+  MeetingIdViewLoading,
+} from "@/modules/meetings/ui/views/meeting-id-view";
+
+interface MeetingIdPageProps {
+  params: Promise<{ meetingId: string }>;
+}
+
+const MeetingIdPage = async ({ params }: MeetingIdPageProps) => {
+  const { meetingId } = await params;
+
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session) {
+    redirect("/sign-in");
+  }
+
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(
+    trpc.meetings.getOne.queryOptions({ id: meetingId }),
+  );
+  // TODO: prefetch `meetings.getTranscript`
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<MeetingIdViewLoading />}>
+        <ErrorBoundary fallback={<MeetingIdViewError />}>
+          <MeetingIdView meetingId={meetingId} />
+        </ErrorBoundary>
+      </Suspense>
+    </HydrationBoundary>
+  );
 };
 
 export default MeetingIdPage;
---
+++ src/modules/meetings/ui/views/meeting-id-view.tsx
@@ -0,0 +1,98 @@
+"use client";
+
+import {
+  useMutation,
+  useQueryClient,
+  useSuspenseQuery,
+} from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+import { useState } from "react";
+
+import { useTRPC } from "@/trpc/client";
+import { LoadingState } from "@/components/loading-state";
+import { ErrorState } from "@/components/error-state";
+import { useConfirm } from "@/hooks/use-confirm";
+
+import { MeetingIdViewHeader } from "../components/meeting-id-view-header";
+import { UpdateMeetingDialog } from "../components/update-meeting-dialog";
+
+interface MeetingIdViewProps {
+  meetingId: string;
+}
+
+export const MeetingIdView = ({ meetingId }: MeetingIdViewProps) => {
+  const [updateMeetingDialogOpen, setUpdateMeetingDialogOpen] = useState(false);
+  const router = useRouter();
+  const queryClient = useQueryClient();
+  const trpc = useTRPC();
+
+  const [RemoveConfirmation, confirmRemove] = useConfirm(
+    "Are you sure?",
+    "The following action will remove this meeting",
+  );
+
+  const { data } = useSuspenseQuery(
+    trpc.meetings.getOne.queryOptions({ id: meetingId }),
+  );
+
+  const removeMeeting = useMutation(
+    trpc.meetings.remove.mutationOptions({
+      onSuccess: () => {
+        queryClient.invalidateQueries(trpc.meetings.getMany.queryOptions({}));
+        // TODO: invalidate free tier usage
+        router.push("/meetings");
+      },
+      onError: (error) => {
+        toast.error(error.message);
+      },
+    }),
+  );
+
+  const handleRemoveMeeting = async () => {
+    const ok = await confirmRemove();
+
+    if (!ok) return;
+
+    await removeMeeting.mutateAsync({ id: meetingId });
+  };
+
+  return (
+    <>
+      <RemoveConfirmation />
+      <UpdateMeetingDialog
+        open={updateMeetingDialogOpen}
+        onOpenChange={setUpdateMeetingDialogOpen}
+        initialValues={data}
+      />
+
+      <div className="flex flex-1 flex-col gap-y-4 p-4 md:px-8">
+        <MeetingIdViewHeader
+          meetingId={meetingId}
+          meetingName={data.name}
+          onEdit={() => setUpdateMeetingDialogOpen(true)}
+          onRemove={handleRemoveMeeting}
+        />
+        {JSON.stringify(data, null, 2)}
+      </div>
+    </>
+  );
+};
+
+export const MeetingIdViewLoading = () => {
+  return (
+    <LoadingState
+      title="Loading Meeting"
+      description="This may take a few seconds"
+    />
+  );
+};
+
+export const MeetingIdViewError = () => {
+  return (
+    <ErrorState
+      title="Error Loading Meeting"
+      description="Please try again later"
+    />
+  );
+};
